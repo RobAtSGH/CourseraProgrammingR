e12fdb67cb7ade40fa9795ca082164322a5bc07c
Rob Novak <robatsgh@gmail.com>
2015-03-09 10:53:45 -0400

    Set up week 2 project

---
+++ week2Project/corr.R
@@ -0,0 +1,11 @@
+corr <- function(directory, threshold = 0) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  print(directory)
+  ## 'threshold' is a numeric vector of length 1 indicating the
+  ## number of completely observed observations (on all
+  ## variables) required to compute the correlation between
+  ## nitrate and sulfate; the default is 0
+  print(threshold)
+  ## Return a numeric vector of correlations
+}
