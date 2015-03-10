d2c5699dfd3e3975b179d010846c7d635aed50c0
Rob Novak <robatsgh@gmail.com>
2015-03-10 11:01:09 -0400

    Created pollutantmean.R

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Repository for Programming R classwork
 * 2015-03-06: Completed week one coursework. Checked in lecture slides, sample code logs from lectures, and week one quiz data/code log.
 * 2015-03-09: Checked in week two lecture slides. Added spec data for week 2 programming project. Removed spaces from directory names. Created project stubs.
 * 2015-03-09: Created flow control examples for if-then-else, for loop, and while loop. Completed scoping and function examples.
+* 2015-03-10: Created pollutantmean() function for week 2 project. Submitted for grading - passed all tests.
--- week2Project/pollutantmean.R
+++ week2Project/pollutantmean.R
@@ -1,14 +1,20 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
+    # 'directory' is a character vector of length 1 indicating
+    # the location of the CSV files
+    # 'pollutant' is a character vector of length 1 indicating
+    # the name of the pollutant for which we will calculate the
+    # mean; either "sulfate" or "nitrate".
+    # 'id' is an integer vector indicating the monitor ID numbers
+    # to be used
 
-  ## 'pollutant' is a character vector of length 1 indicating
-  ## the name of the pollutant for which we will calculate the
-  ## mean; either "sulfate" or "nitrate".
+    # First, create the list of files to open.
+    files <- paste(directory,"/",formatC(id,width=3,format="d",flag="0"),".csv",sep="")
+    # Open all the files as a list
+    airquality <- lapply(files,read.csv)
+    # Collapse the list into a single large dataframe
+    airquality <- do.call(rbind,airquality)
 
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
-
-  ## Return the mean of the pollutant across all monitors list
-  ## in the 'id' vector (ignoring NA values)
+    ## Return the mean of the pollutant across all monitors list
+    ## in the 'id' vector (ignoring NA values)
+    signif(mean(airquality[,pollutant],na.rm=TRUE),digits=4)
 }
